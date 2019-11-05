a24047c137ff6259522858b58390520783bd74ce
kunal.vaidya <kunal.vaidya@compliantia.net>
2019-11-05 10:31:40 -0500

    Added new component for the edit modal

--- src/components/CustomerCard.vue
+++ src/components/CustomerCard.vue
@@ -1,79 +1,35 @@
 <template>
-  <div class="customer-card text-left">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">{{customer.name}}</h5>
-        <h6 class="card-subtitle mb-3 text-muted">{{customer.email}}</h6>
-        <p class="card-text"><b>Address: </b> {{customer.address}}, {{customer.city}} {{customer.postal}}, {{customer.province}} {{customer.country}}</p>
-        <button type ="button" class="btn btn-lg btn-warning" data-toggle="modal" data-target="#edit-information">EDIT</button>
-      </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="edit-information" tabindex="-1" role="dialog" aria-labelledby="edit-information-label" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="edit-information-label">Edit Customer Information</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            
-            <form>
-              <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                  <input type="text" v-model="name" class="form-control-plaintext">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                  <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-                </div>
-              </div>
-            </form>
-
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div><!--/ customer-card -->
+	<div class="customer-card text-left">
+		<div class="card">
+			<div class="card-body">
+				<h5 class="card-title">{{customer.name}}</h5>
+				<h6 class="card-subtitle mb-3 text-muted">{{customer.email}}</h6>
+				<p class="card-text"><b>Address: </b> {{customer.address}}, {{customer.city}} {{customer.postal}}, {{customer.province}} {{customer.country}}</p>
+				<button type ="button" class="btn btn-lg btn-warning" data-toggle="modal" data-target="#edit-information">EDIT</button>
+			</div>
+		</div>
+		<CustomerEditModal :customer="customer"/>
+	</div><!--/ customer-card -->
 </template>
 
 <script>
+import CustomerEditModal from '@/components/CustomerEditModal.vue'
+
 export default {
-  name: 'CustomerCard',
-  data() {
-    return {
-      name: this.customer.name,
-      email: this.customer.email,
-      channel: this.customer.channel,
-      address: this.customer.address,
-      postal: this.customer.postal,
-      city: this.customer.city,
-      province: this.customer.province,
-      country: this.customer.country
-    }
-  },
-  props: {
-    customer: {
-      type: Object,
-      default: null
-    }
-  }
+	name: 'CustomerCard',
+	components: { CustomerEditModal },
+	props: {
+		customer: {
+			type: Object,
+			default: null
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 .card {
-  margin-top: 20px;
-  margin-bottom: 20px;
+	margin-top: 20px;
+	margin-bottom: 20px;
 }
 </style>
---
+++ src/components/CustomerEditModal.vue
@@ -0,0 +1,99 @@
+<template>
+	<!-- Modal -->
+	<div class="modal fade" id="edit-information" tabindex="-1" role="dialog" aria-labelledby="edit-information-label" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="edit-information-label">Edit Customer Information</h5>
+					<button type="button" class="close" @click="secondaryAction" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					
+					<form>
+						<div class="form-group row">
+							<label class="col-sm-2 col-form-label">Name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="updatedModel.name" class="form-control-plaintext">
+							</div>
+						</div>
+						<div class="form-group row">
+							<label class="col-sm-2 col-form-label">Email</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="updatedModel.email" class="form-control-plaintext">
+							</div>
+						</div><div class="form-group row">
+							<label class="col-sm-2 col-form-label">Channel</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="updatedModel.channel" class="form-control-plaintext">
+							</div>
+						</div>
+					</form>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" @click="secondaryAction">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default  {
+	name: 'CustomerEditModal',
+	data() {
+		return {
+			model: {
+				name: this.customer.name,
+				email: this.customer.email,
+				channel: this.customer.channel,
+				address: this.customer.address,
+				postal: this.customer.postal,
+				city: this.customer.city,
+				province: this.customer.province,
+				country: this.customer.country
+			},
+			updatedModel: {
+				name: '',
+				email: '',
+				channel: '',
+				address: '',
+				postal: '',
+				city: '',
+				province: '',
+				country: ''
+			}
+		}
+	},
+	props: {
+		customer: {
+			type: Object,
+			default: null
+		}
+	},
+	mounted () {
+		if ( this.model ) {
+			Object.assign(this.updatedModel, this.model)
+		}
+	},
+	methods: {
+		primaryAction () {
+
+		},
+		/**
+		 * @description Method to carry out the functions for seconday button click. 
+		 * - Closes the Modal.
+		 */
+		secondaryAction () {
+			$('#edit-information').modal('hide')
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
